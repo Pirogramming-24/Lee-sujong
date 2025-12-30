c85dfb7bac9573bf0cb73899a4e942a2d2df9af3
issuejong <lsj63223@naver.com>
2025-12-30 23:35:10 +0900

    feat: db 세팅

--- myMovieReviews/reviews/models.py
+++ myMovieReviews/reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+    director = models.CharField(max_length=100)
+    actors = models.CharField(max_length=200)
+    runtime = models.IntegerField()
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
