fac24180562e4b6152c1a63a9b6cfda647b76bcc
issuejong <lsj63223@naver.com>
2025-12-31 11:08:55 +0900

    feat: review_detail 기능 구현

--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -3,3 +3,7 @@ from django.http import JsonResponse
 def review_list(request):
     reviews = Review.objects.all()
     return render(request, "reviews/list.html", {"review" : reviews})
+
+def review_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, "reviews/detail.html", {"review": review})
