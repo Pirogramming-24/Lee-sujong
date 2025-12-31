addaa37383080d78d39d8a608544d407c197e226
issuejong <lsj63223@naver.com>
2025-12-31 11:45:44 +0900

    feat: review_update 기능 구현

--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -9,3 +9,25 @@ def review_list(request):
 def review_detail(request, pk):
     review = get_object_or_404(Review, pk=pk)
     return render(request, "reviews/detail.html", {"review": review})
+
+def review_create(request):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            if form.is_valid():
+                form.save()
+                return redirect("reviews:list")
+    else:
+        form = ReviewForm()
+    return render(request, "reviews/form.html", {"form": form, "mode": "create"})
+
+def review_update(request, pk):
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance = review)
+        if form.is_valid():
+            if form.is_valid():
+                form.save()
+                return redirect("reviews:list", pk=review.pk)
+    else:
+        form = ReviewForm(instance = review)
+    return render(request, "reviews/form.html", {"form": form, "mode": "create"})
