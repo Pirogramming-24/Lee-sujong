8d60e223df87de8d7a1cb12edf92eba33dbf3a21
issuejong <lsj63223@naver.com>
2026-01-01 22:46:38 +0900

    feat: ideas.urls.py 추가

--- SWIDEA_SITE/ideas/urls.py
+++ SWIDEA_SITE/ideas/urls.py
@@ -4,5 +4,11 @@ from .import views
 app_name = "ideas"
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.main, name="main"),
+    path("ideas/create/", views.idea_create, name="create"),
+    path("ideas/<int:pk>/", views.idea_detail, name="detail"), 
+    path("ideas/<int:pk>/update/", views.idea_update, name="update"),
+    path("ideas/<int:pk>/delete/", views.idea_delete, name="delete"),
+    path("ideas/<int:pk>/toggle-star/", views.idea_toggle_star, name="toggle"),
+    path("ideas/<int:pk>/interest/", views.idea_interest, name="interest"),
 ]
