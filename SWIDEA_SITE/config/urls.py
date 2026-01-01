1f5c8be523f35bf67255a5c53ac13ae0521c8a7e
issuejong <lsj63223@naver.com>
2026-01-01 22:48:08 +0900

    chore: urlpatterns 재설정 및 devtools app 추가

--- SWIDEA_SITE/config/urls.py
+++ SWIDEA_SITE/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from ideas import views as idea
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("ideas.urls")),
+    path("", idea.main, name="home"),
+    path("ideas/", include("ideas.urls")),
+    path("devtools/", include("devtools.urls")),
 ]
