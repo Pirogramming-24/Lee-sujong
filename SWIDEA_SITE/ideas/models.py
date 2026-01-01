127629c49fb34ec32442c7480fae2d5a8beee262
issuejong <lsj63223@naver.com>
2026-01-01 22:28:46 +0900

    feat: model 추가

--- SWIDEA_SITE/ideas/models.py
+++ SWIDEA_SITE/ideas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class DevTool(models.Model):
+    name = models.CharField(max_length=100)
+    kind = models.CharField(max_length=100)
+    content = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Idea(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="ideas/", blank=True, null=True)
+    content = models.CharField(max_length=300)
+    interest = models.IntegerField(default=0)
+    devtool = models.ForeignKey(DevTool, on_delete=models.CASCADE, related_name="ideas")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class IdeaStar(models.Model):
+    idea = models.OneToOneField(Idea, on_delete=models.CASCADE, related_name="star")
+    is_starred = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"⭐ {self.idea.title} ({self.is_starred})"
