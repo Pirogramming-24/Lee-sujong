abce130c2ceb6acafab948aa5a7fffd15570ba00
issuejong <lsj63223@naver.com>
2026-01-01 23:32:51 +0900

    feat: idea.urls.py 수정

--- SWIDEA_SITE/ideas/urls.py
+++ SWIDEA_SITE/ideas/urls.py
@@ -4,11 +4,10 @@ from .import views
 app_name = "ideas"
 
 urlpatterns = [
-    path("", views.main, name="main"),
-    path("ideas/create/", views.idea_create, name="create"),
-    path("ideas/<int:pk>/", views.idea_detail, name="detail"), 
-    path("ideas/<int:pk>/update/", views.idea_update, name="update"),
-    path("ideas/<int:pk>/delete/", views.idea_delete, name="delete"),
-    path("ideas/<int:pk>/toggle-star/", views.idea_toggle_star, name="toggle"),
-    path("ideas/<int:pk>/interest/", views.idea_interest, name="interest"),
+    path("create/", views.idea_create, name="create"),
+    path("<int:pk>/", views.idea_detail, name="detail"),
+    path("<int:pk>/update/", views.idea_update, name="update"),
+    path("<int:pk>/delete/", views.idea_delete, name="delete"),
+    path("<int:pk>/toggle-star/", views.idea_toggle_star, name="toggle"),
+    path("<int:pk>/interest/", views.idea_interest, name="interest"),
 ]
