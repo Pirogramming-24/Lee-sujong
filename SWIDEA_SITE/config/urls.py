73679d142b8854bdb25e744d085cb89bb836bd34
issuejong <lsj63223@naver.com>
2026-01-02 20:43:06 +0900

    feat: 이미지 기능 추가

--- SWIDEA_SITE/config/urls.py
+++ SWIDEA_SITE/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from ideas import views as idea
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path("ideas/", include("ideas.urls")),
     path("devtools/", include("devtools.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
